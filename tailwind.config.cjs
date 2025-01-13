65321c218765849d7973b854e13af27f9ae86f98
pink10000 <kyletrinh1223@gmail.com>
2025-01-12 21:22:21 -0800

    add dark mode :3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-    "./public/**/*.html",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}", "./public/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        mono: ['monospace'],
+        mono: ["monospace"],
+      },
+      colors: {
+        dark: {
+          background: '#121212',
+        },
+      },
+      transitionDuration: {
+        'default': '200ms',
       },
     },
   },

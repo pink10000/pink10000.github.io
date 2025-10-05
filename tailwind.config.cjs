99eab2b3c2c68d5623f9e70fd767ddb9d30c8e79
pink10000 <kyletrinh1223@gmail.com>
2025-10-05 14:52:29 -0700

    dark oxocarbon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+// uses Oxocarbon Colorscheme
+// https://github.com/nyoom-engineering/base16-oxocarbon
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}", "./public/**/*.html"],
@@ -10,6 +13,40 @@ module.exports = {
       colors: {
         dark: {
           background: '#161616',
+          base00: '#161616',
+          base01: '#262626',
+          base02: '#393939', 
+          base03: '#525252', 
+          base04: '#dde1e6', 
+          base05: '#f2f4f8',
+          base06: '#ffffff', 
+          base07: '#08bdba',
+          base08: '#3ddbd9', 
+          base09: '#78a9ff', 
+          base0A: '#ee5396', 
+          base0B: '#33b1ff', 
+          base0C: '#ff7eb6', 
+          base0D: '#42be65', 
+          base0E: '#be95ff', 
+          base0F: '#82cfff',
+        },
+        light: {
+          base00: '#f2f4f8',
+          base01: '#dde1e6',
+          base02: '#525252',
+          base03: '#161616',
+          base04: '#262626',
+          base05: '#393939',
+          base06: '#525252',
+          base07: '#08bdba',
+          base08: '#ff7eb6',
+          base09: '#ee5396',
+          base0A: '#FF6F00',
+          base0B: '#0f62fe',
+          base0C: '#673AB7',
+          base0D: '#42be65',
+          base0E: '#be95ff',
+          base0F: '#37474F',
         },
       },
       transitionDuration: {

f422da87c19c49a82799d8b15e72a28fd0098a92
pink10000 <kyletrinh1223@gmail.com>
2026-05-30 23:50:16 -0700

    latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pink10000.github.io",
   output: "static",
   integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   vite: {
     server: {
       headers: {
